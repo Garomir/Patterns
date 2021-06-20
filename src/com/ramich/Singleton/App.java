35d60425938cebd0b54a3d97baafb829fe659ec4
Ramil Safuanov <ramilfin@mail.ru>
2021-06-20 10:34:20 +0300

    Added pattern Singleton.

---
+++ src/com/ramich/Singleton/App.java
@@ -0,0 +1,11 @@
+package com.ramich.Singleton;
+
+public class App {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        System.out.println(singleton1);
+
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton2);
+    }
+}
